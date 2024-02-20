9cb015314e156b78aeecf6563b5bdd1968870ea3
Zahalka Nicholas <st61057@upce.cz>
2024-02-20 14:38:48 +0100

    First commit

---
+++ src/test/java/com/example/NNPIA_CV02/NnpiaCv02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NNPIA_CV02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaCv02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
